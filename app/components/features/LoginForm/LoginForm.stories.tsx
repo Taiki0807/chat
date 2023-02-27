9ce686f3eb4b821f6222a36cd59a51736428a8fc
Taiki0807 <hosokawataiki265@gmail.com>
2023-02-28 05:18:54 +0900

    feat: add tests for login form

--- app/components/features/LoginForm/LoginForm.stories.tsx
+++ app/components/features/LoginForm/LoginForm.stories.tsx
@@ -2,7 +2,12 @@ import {
   type ComponentMeta,
   type ComponentStoryObj,
 } from '@storybook/react';
+import {
+  within,
+  userEvent,
+} from '@storybook/testing-library';
 import { LoginForm } from './LoginForm';
+import { AuthProvider } from '@/app/components/features/LoginForm/AuthContext';
 
 type T = typeof LoginForm;
 type Meta = ComponentMeta<T>;
@@ -11,6 +16,31 @@ type Story = ComponentStoryObj<T>;
 export default {
   title: 'LoginForm',
   component: LoginForm,
+  decorators: [
+    (Story) => {
+      return (
+        <AuthProvider>
+          <Story />
+        </AuthProvider>
+      );
+    },
+  ],
 } as Meta;
 
-export const Default: Story = {};
+export const EmptyForm: Story = {};
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.type(
+      canvas.getByTestId('username'),
+      'username'
+    );
+
+    await userEvent.type(
+      canvas.getByTestId('password'),
+      'a-random-password'
+    );
+    await userEvent.click(canvas.getByRole('button'));
+  },
+};
