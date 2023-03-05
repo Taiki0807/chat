0c707b65063b2758b1033f295c9882f2ce955f23
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-06 02:57:21 +0900

    feat: add msw-storybook-addon initialization

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,18 @@
 import { addDecorator } from '@storybook/react';
 import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
+import {
+  initialize,
+  mswDecorator,
+} from 'msw-storybook-addon';
 
 addDecorator((Story) => (
   <MemoryRouterProvider>
     <Story />
   </MemoryRouterProvider>
 ));
+initialize();
 
+export const decorators = [mswDecorator];
 export const parameters = {
   nextjs: {
     appDirectory: true,
