74b2af7acc78c60a00a9670d14bd129d0c4f2b63
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-06 11:40:46 +0900

    feat: add alias for next/router

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,6 +24,7 @@ module.exports = {
         alias: {
           ...config.resolve.alias,
           '@': path.resolve(__dirname, '../'),
+          'next/router': 'next-router-mock',
         },
       },
     };
