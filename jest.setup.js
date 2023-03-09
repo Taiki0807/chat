0b95ce50224e5d3c43a4bce4b35b39074fd61b78
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-09 13:48:31 +0900

    feat(testing-react): replace deprecated setGlobalConfig with setProjectAnnotations

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,6 @@
-import { setGlobalConfig } from '@storybook/testing-react';
+import { setProjectAnnotations } from '@storybook/testing-react';
 import * as globalStorybookConfig from './.storybook/preview';
 import 'whatwg-fetch';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-setGlobalConfig(globalStorybookConfig);
+setProjectAnnotations(globalStorybookConfig);
