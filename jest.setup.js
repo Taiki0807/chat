2b0faaa496d24b2b85e8f01fed91891de8be1af6
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-14 00:48:56 +0900

    feat: update import for setProjectAnnotations

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,4 @@
-import { setProjectAnnotations } from '@storybook/testing-react';
+import { setProjectAnnotations } from '@storybook/react';
 import * as globalStorybookConfig from './.storybook/preview';
-import 'whatwg-fetch';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 setProjectAnnotations(globalStorybookConfig);
