4b5f1aa5daaf85837843c2ea21d18555991918e2
Taiki0807 <hosokawataiki265@gmail.com>
2023-06-10 15:22:10 +0900

    feat: add online, width, and height properties

--- app/components/parts/Icon/Icon.tsx
+++ app/components/parts/Icon/Icon.tsx
@@ -4,6 +4,9 @@ import style from './Icon.module.css';
 interface Props {
   img: string;
   alt: string;
+  online: boolean;
+  width: number;
+  height: number;
 }
 
 export const Icon = (props: Props): JSX.Element => {
@@ -12,11 +15,15 @@ export const Icon = (props: Props): JSX.Element => {
       <Image
         src={props.img}
         alt={props.alt}
-        width={60}
-        height={60}
+        width={props.width}
+        height={props.height}
         className={style.icon}
       />
-      <div className={style.mark}></div>
+      {props.online ? (
+        <div className={style.mark}></div>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
