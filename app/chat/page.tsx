c5a5cf7fc488c08a5503286209f4f60099b69d26
Taiki0807 <hosokawataiki265@gmail.com>
2023-06-10 15:43:54 +0900

    feat: add chat top page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import { ChatTop } from '../components/pages/ChatTop';
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <ChatTop />
+    </div>
+  );
 };
 
 export default page;
