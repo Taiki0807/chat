0c47c0b3a5e9203bf993f9c767f90aaad31b084e
Taiki0807 <hosokawataiki265@gmail.com>
2023-06-10 16:09:16 +0900

    feat: add Popover component's Storybook and test files

---
+++ app/components/parts/Popover/Popover.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import { Popover } from './Popover';
+
+const meta = {
+  title: 'Popover',
+  component: Popover,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Popover>;
+
+export default meta;
+type T = typeof Popover;
+type Story = StoryObj<T>;
+
+const ButtonWithHooks = () => {
+  const [anchorEl, setAnchorEl] =
+    useState<HTMLButtonElement | null>(null);
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setOpen(!open);
+    setAnchorEl(event.currentTarget);
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>Open Popover</button>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+      >
+        popover
+      </Popover>
+    </div>
+  );
+};
+export const Default: Story = {
+  render: () => <ButtonWithHooks />,
+};
